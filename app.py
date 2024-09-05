20e6e9906f356d7b7b05ecc3e01bf7f363fd8eb6
Sergio Perez <se.perezr12@uniandes.edu.co>
2024-09-04 21:11:07 -0500

    Simulacion de fallo

--- app.py
+++ app.py
@@ -12,17 +12,28 @@ with open('config.json', 'r') as f:
 primary_service = config['primary_service_url']
 backup_service = config['backup_service_url']
 retry_count = config['retry_count']
+simulate_error_count =  config['simulate_error_count']
+call_counter = 0
+current_service = primary_service
 
 
 def monitor(url):
+    global call_counter
+    print(f"Call counter {call_counter}")
 
     for attempt in range(retry_count):
         print(f"LLamando servicio {url}")
         try:
             response = requests.get(url)
             if response.status_code == 200:
-                print("Respuesta exitosa")
-                return response
+
+                call_counter += 1
+                if call_counter >= simulate_error_count:
+                    print(f"Simulando fallo")
+                    print(f"Intento {attempt + 1} fallido")
+                else:
+                    print("Respuesta exitosa")
+                    return response
             else:
                 print(f"Intento {attempt + 1} fallido")
         except Exception as e:
@@ -35,16 +46,20 @@ def monitor(url):
 # Ruta del API Gateway
 @app.route('/gateway', methods=['GET'])
 def gateway():
-    global current_service
+    global current_service, call_counter
 
-    service_url = primary_service
-    response = monitor(service_url)
+    response = monitor(current_service)
 
     if response is None:
         print("Cambiando al servicio de respaldo...")
-        service_url = backup_service
-        response = monitor(service_url)
-        
+        call_counter = 0
+        if current_service == primary_service:
+            current_service=backup_service
+        else:
+            current_service=primary_service
+        response = monitor(current_service)
+
+
         if response is None:
             return jsonify({"error": "Todos los servicios fallaron"}), 503
     
