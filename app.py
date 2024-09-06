6bb2833e3fdc9a2849fb2b369ead239dba13a3e8
Sergio Perez <se.perezr12@uniandes.edu.co>
2024-09-06 16:55:34 -0500

    Escribir logs y tiempo entre reintentos

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import csv
+import datetime
 import requests
 from flask import Flask, jsonify
 import json
@@ -12,98 +14,62 @@ with open('config.json', 'r') as f:
 primary_service = config['primary_service_url']
 backup_service = config['backup_service_url']
 retry_count = config['retry_count']
-simulate_error_count =  config['simulate_error_count']
 call_counter = 0
 current_service = primary_service
 
 
+def write_log(service_name, status_code, message):
+    log_file = 'gateway_logs.csv'
+    with open(log_file, mode='a', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow([datetime.datetime.now(), service_name, status_code, message])
+
 def monitor(url):
-    global call_counter
-    print(f"Call counter {call_counter}")
-
-    for attempt in range(retry_count):
-        print(f"LLamando servicio {url}")
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-
-                call_counter += 1
-                if call_counter >= simulate_error_count:
-                    print(f"Simulando fallo")
-                    print(f"Intento {attempt + 1} fallido")
-                else:
-                    print("Respuesta exitosa")
-                    return response
-            else:
-                print(f"Intento {attempt + 1} fallido")
-        except Exception as e:
-            print(f"Intento {attempt + 1} fallido: {e}")
-        
-        time.sleep(1)
-
-    return None
-
-def monitor2(url):
     for attempt in range(retry_count):
         print(f"Reintentando llamado al servicio {url}")
+        write_log(url, "", 'Reintento Api Call')
         response = requests.get(url)
         if response.status_code == 200:
+            write_log(url, response.status_code, 'Respuesta exitosa')
             return response
         
         attempt += 1
-        time.sleep(0.5)
+        time.sleep(0.2)
 
+    write_log(url, response.status_code, 'Fallo Api Call')
     return None
 
-
-# Ruta del API Gateway
-@app.route('/gateway', methods=['GET'])
-def gateway():
-    global current_service, call_counter
-
-    response = monitor(current_service)
-
-    if response is None:
-        print("Cambiando al servicio de respaldo...")
-        call_counter = 0
-        if current_service == primary_service:
-            current_service = backup_service
-        else:
-            current_service = primary_service
-        
-        response = monitor(current_service)
-
-        if response is None:
-            return jsonify({"error": "Todos los servicios fallaron"}), 503
-    
-    return jsonify(response.json()), response.status_code
 
 # Ruta del API Gateway
 @app.route('/gateway/v2', methods=['GET'])
 def gateway2():
     global current_service
 
+    write_log(current_service, 'N/A', 'Api Call')
     response = requests.get(current_service)
-
+    
     if (response.status_code == 200):
+        write_log(current_service, response.status_code, 'Respuesta exitosa')
         return jsonify(response.json()), response.status_code
     else: 
-        response = monitor2(current_service)
+        response = monitor(current_service)
 
         if response is None:
             print("Cambiando al servicio de respaldo...")
-            
             if current_service == primary_service:
+                write_log(f'{primary_service} -> {backup_service}', '', 'Cambio de servicio')
                 current_service = backup_service
             else:
+                write_log(f'{backup_service} -> {primary_service}', '', 'Cambio de servicio')
                 current_service = primary_service
                 
             response = requests.get(current_service)
 
             if (response.status_code == 200):
                 return jsonify(response.json()), response.status_code
-
+            
+        write_log(current_service, "", 'Todos los servicios fallaron')
         return jsonify({"error": "Todos los servicios fallaron"}), 503
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(port=5000)
